8dc2f85e5b72f1872ff697d4736dbd015af85468
Sbjoel <joelyahaya7@gmail.com>
2026-06-16 15:43:33 +0100

    Latest commit

--- vite.config.ts
+++ vite.config.ts
@@ -25,6 +25,7 @@ export default defineConfig(({ mode }) => {
       host: "0.0.0.0",
       port: process.env.PORT ? parseInt(process.env.PORT) : 8080,
       strictPort: true,
+      allowedHosts: "all",
     },
     plugins: [react()],
     resolve: {
